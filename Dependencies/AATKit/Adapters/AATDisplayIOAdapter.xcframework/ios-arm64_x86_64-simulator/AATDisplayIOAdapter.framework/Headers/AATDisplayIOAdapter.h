08702005865628a7ae7500e6571671698ee549de
Mohamed Matloub <Mohamed.a.matloub@gmail.com>
2025-01-29 13:14:43 +0100

    Implement display.io

---
+++ Dependencies/AATKit/Adapters/AATDisplayIOAdapter.xcframework/ios-arm64_x86_64-simulator/AATDisplayIOAdapter.framework/Headers/AATDisplayIOAdapter.h
@@ -0,0 +1,19 @@
+//
+//  AATDisplayIOAdapter.h
+//  AATDisplayIOAdapter
+//
+//  Created by Mohamed Matloub  on 29.01.25.
+//  Copyright © 2025 AddApptr GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AATDisplayIOAdapter.
+FOUNDATION_EXPORT double AATDisplayIOAdapterVersionNumber;
+
+//! Project version string for AATDisplayIOAdapter.
+FOUNDATION_EXPORT const unsigned char AATDisplayIOAdapterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AATDisplayIOAdapter/PublicHeader.h>
+
+
