5b855914ee1ff942a42789787e728984ba0192c8
Mahmoud Amer <mahmoud.amer@addapptr.com>
2025-04-30 15:27:01 +0200

    Release 3.12.5

--- Package.swift
+++ Package.swift
@@ -77,7 +77,7 @@ let package = Package(
         .package(url: "https://github.com/googleads/swift-package-manager-google-mobile-ads.git", exact: Version(12, 2, 0)),
         .package(url: "https://github.com/GeoEdgeSDK/AppHarbrSDK.git", exact: Version(1, 21, 0)),
         .package(url: "https://github.com/googleads/swift-package-manager-google-user-messaging-platform.git", "1.1.0"..<"4.0.0"),
-        .package(url: "https://github.com/AddApptr/RTBSPM.git", exact: Version(1, 6, 2)),
+        .package(url: "https://github.com/AddApptr/RTBSPM.git", exact: Version(1, 6, 3)),
 //        .package(name: "TeadsSDK", url: "https://github.com/teads/TeadsSDK-iOS", .exact("5.1.3")), Conflict in AppLovin dependency
     ],
     targets: [
